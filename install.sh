d2b77ee1cd72115af821f7aae1be3b3a1157c5ce
muflikhandimasd <muflikhan.dimas@gmail.com>
2024-10-22 17:22:56 +0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo chmod 0777 system/storage/cache/
+	sudo	chmod 0777  system/storage/download/
+	sudo	chmod 0777  system/storage/logs/
+	sudo	chmod 0777  system/storage/modification/
+	sudo	chmod 0777  system/storage/session/
+	sudo	chmod 0777  system/storage/upload/
+	sudo	chmod 0777  system/storage/vendor/
+	sudo	chmod 0777  image/
+	sudo	chmod 0777  image/cache/
+	sudo	chmod 0777  image/catalog/
+	sudo	chmod 0777  config.php
+	sudo	chmod 0777  admin/config.php
